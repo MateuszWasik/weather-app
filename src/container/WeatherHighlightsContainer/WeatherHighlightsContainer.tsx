421466dc2b83c81ee5e98627e00cc7ae00cd96a5
Mateusz Wasik <wasik.matt@gmail.com>
2021-03-09 19:19:08 +0100

    add icon, prepare sunrise/sunset date for component, sass styles

--- src/container/WeatherHighlightsContainer/WeatherHighlightsContainer.tsx
+++ src/container/WeatherHighlightsContainer/WeatherHighlightsContainer.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import WindComponent from 'component/WindComponent/WindComponent'
+import { SunriseSunsetComponent } from '../../component/SunriseSunsetComponent/SunriseSunsetComponent'
+import './WeatherHighligthsContainer.sass'
 
 
 const WeatherHighlightsContainer: React.FC<any> = ({data}) => {
   return(
     <div className="weather-highlights-container">
       <WindComponent windSpeed={data.windSpeed} windDeg={data.windDeg} />
+      <SunriseSunsetComponent sunrise={data.sunrise} sunset={data.sunset} />
 
     </div>
   )
