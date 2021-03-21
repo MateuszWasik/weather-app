55c23bc72e182e47bda556bf10053d5d859a8fde
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-21 16:32:37 +0000

    Deploy codrill/weather-app to github.com/codrill/weather-app.git:master

---
+++ precache-manifest.763fa5128e9367a78714c126d559f284.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a303e07b4150712d18851790637a4ab",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "e8be6034eaa7cf628f44",
+    "url": "/weather-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8b504da007652fc99a2d",
+    "url": "/weather-app/static/css/main.76ac0772.chunk.css"
+  },
+  {
+    "revision": "e8be6034eaa7cf628f44",
+    "url": "/weather-app/static/js/2.68a5eef9.chunk.js"
+  },
+  {
+    "revision": "4aac913929d24132b929af181f200ce4",
+    "url": "/weather-app/static/js/2.68a5eef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b504da007652fc99a2d",
+    "url": "/weather-app/static/js/main.0d27dd36.chunk.js"
+  },
+  {
+    "revision": "12ef64cf8e1aad5a2dd2",
+    "url": "/weather-app/static/js/runtime-main.c90f5b59.js"
+  },
+  {
+    "revision": "bd652b9ee30ed7c7feb9c7cd66e73f2e",
+    "url": "/weather-app/static/media/broken-clouds.bd652b9e.svg"
+  },
+  {
+    "revision": "632833f9eb14c688b76b8d2504e501c8",
+    "url": "/weather-app/static/media/clear-night.632833f9.svg"
+  },
+  {
+    "revision": "346b346f5561d4876d9dede130c3b66a",
+    "url": "/weather-app/static/media/clear-sky.346b346f.svg"
+  },
+  {
+    "revision": "00ca63bc4392c0d662571d749858f0ee",
+    "url": "/weather-app/static/media/cloudy-night.00ca63bc.svg"
+  },
+  {
+    "revision": "7ee70f9fbfac3ec8b5360a4b53007c00",
+    "url": "/weather-app/static/media/cloudy.7ee70f9f.svg"
+  },
+  {
+    "revision": "aa731732b5897d3615f66364f4c2ed3c",
+    "url": "/weather-app/static/media/compass.aa731732.svg"
+  },
+  {
+    "revision": "7850bef1e7dea7635a0202b3a4eb6ebd",
+    "url": "/weather-app/static/media/fog.7850bef1.svg"
+  },
+  {
+    "revision": "418c454c47665be2542b9c39ce420227",
+    "url": "/weather-app/static/media/rainy-day.418c454c.svg"
+  },
+  {
+    "revision": "d23b18eae363626eb655ed449ecfd203",
+    "url": "/weather-app/static/media/rainy-night.d23b18ea.svg"
+  },
+  {
+    "revision": "e9ee5d2d8f0c7c29b2ff4b8ccca9a460",
+    "url": "/weather-app/static/media/small-cloud.e9ee5d2d.svg"
+  },
+  {
+    "revision": "058622dea06415696615842285390d2c",
+    "url": "/weather-app/static/media/snowy-night.058622de.svg"
+  },
+  {
+    "revision": "b3673467354aa397d42e76e43a8a56bf",
+    "url": "/weather-app/static/media/snowy.b3673467.svg"
+  },
+  {
+    "revision": "da61a68ba7db81c38519781d1240b054",
+    "url": "/weather-app/static/media/sunset-sunrise.da61a68b.svg"
+  },
+  {
+    "revision": "f50dfdb7787fe912542321d10ea5a44c",
+    "url": "/weather-app/static/media/thunderstorm.f50dfdb7.svg"
+  }
+]);
